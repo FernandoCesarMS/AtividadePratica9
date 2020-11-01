8d6c5ba860798d91f5212ff3b542c9075604aeea
Fernando Cesar <cmnando.silva@gmail.com>
2020-11-01 19:16:07 -0300

    Func da senoide feita

--- Integral.cpp
+++ Integral.cpp
@@ -51,7 +51,11 @@ class Senoide : public Funcao
 public:
     Senoide();
     double getIntegral(double limiteInferior, double limiteSuperior, double intervalos);
-    double func(double input);
+    double func(double input){
+        double p1 = sin(input);
+        double p2 = input;
+        return p1/p2;
+    }
 };
 class Linear : public Funcao
 {
